e64163c9dfe3219e2885bebb3d44f0b1d399f860
Pinto05Juan <juanmartinpinto30@gmail.com>
2024-07-03 15:54:12 -0300

    Funcionalidad para el componente chat

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
+import { ChatComponent } from './components/chat/chat.component';
 
 
 
 export const routes: Routes = [
-    
+    {
+        path: "chat",
+        component: ChatComponent
+    }
 ]
 
 
