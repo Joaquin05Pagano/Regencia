7b708e48e95e4f14f54e8e4e8e684f89291cec0d
PintoJuan <juanmartinpinto30@gmail.com>
2024-09-16 17:15:25 -0300

    Cambio de la interfaz del chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./components/login/login.component";
 import { ChatComponent } from './components/chat/chat.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, LoginComponent, ChatComponent]
+    imports: [RouterOutlet, LoginComponent, ChatComponent, CommonModule]
 })
 
 export class AppComponent {
   title = 'Regencia'
+  isLoggedIn = false
 }
