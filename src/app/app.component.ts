bd88c4623f53445ca120a2ab3aa9a6def5ca0460
PintoJuan <juanmartinpinto30@gmail.com>
2024-09-22 12:04:40 -0300

    Agregue el componente Informe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./components/login/login.component";
 import { ChatComponent } from './components/chat/chat.component';
 import { CommonModule } from '@angular/common';
+import { InformeComponent } from "./components/informe/informe.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, LoginComponent, ChatComponent, CommonModule]
+    imports: [RouterOutlet, LoginComponent, ChatComponent, CommonModule, InformeComponent]
 })
 
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { ChatComponent } from './components/chat/chat.component';
 
 
-
 export const routes: Routes = [
     {
         path: "chat",
